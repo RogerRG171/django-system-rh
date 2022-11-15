bac3cff023685b2fcbd5e68e734bd58af6e0d89f
roger <roger.goulart.nascimento@gmail.com>
2022-11-15 20:57:52 -0300

    update index home

--- seletive/urls.py
+++ seletive/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from empresa import views
 
 urlpatterns = [
+    path('', views.empresas),
     path('admin/', admin.site.urls),
     path('home/', include('empresa.urls')),
     path('vagas/', include('vagas.urls'))
